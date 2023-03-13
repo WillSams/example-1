01b4d7cd1e55f08de89048fb677f89cab1f07dd9
Will Sams <william.sams@wematchwell.com>
2023-03-13 10:06:13 -0400

    Fixed app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const server = http.createServer((req, res) => {
     res.end('Example-1 Application - Hello, worldi! Test change with Argo CD (via PR merge) part 3\n');
 });
 
-server.listen(port, hostname, () => {
-console.log(`Server running at http://localhost:${port}/`);
+server.listen(port, () => {
+    console.log(`Port forwarded server running at http://localhost:${port}/`);
 });
